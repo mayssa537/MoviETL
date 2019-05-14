1d72a44fb3fecc3e40e733d20082912e2fcf754a
Hamza Sakr <hmzskr@gmail.com>
2019-05-13 23:03:38 -0700

    code clean up

--- Script.sql
+++ Script.sql
@@ -1,8 +1,10 @@
--- CREATE DATABASE movie_db
+-- Run before SQL Alchmy
+CREATE DATABASE movie_db;
 
+-- Run after creating tables for clean up
 USE movie_db;
 
 DELETE FROM movie_db.movie where title IS NULL;
 
 ALTER TABLE `movie_db`.`movie` 
-CHANGE COLUMN `release_date` `release_date` DATETIME NULL DEFAULT NULL ;
+CHANGE COLUMN `release_date` `release_date` DATETIME NULL DEFAULT NULL;
